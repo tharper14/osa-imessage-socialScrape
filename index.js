cfea676446e88ec9833b2dfc446fe35b3103f4e6
Tyler Harper <85406140+tharper14@users.noreply.github.com>
2022-06-11 19:38:48 -0400

    getRecent Update, date format fixed

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const linkPath = '/Users/socialscrape/Social Wake Dropbox/Social Scrape/testLinks.txt' 
-const logPath = '/Users/socialscrape/Social Wake Dropbox/Social Scrape/testLog.txt'
+const linkPath = '/Users/socialscrape/Social Wake Dropbox/Tylers Tests/chatScrape.txt'
+const logPath = '/Users/socialscrape/Social Wake Dropbox/Tylers Tests/chatLog.txt'
+// const linkPath = '/Users/socialscrape/Social Wake Dropbox/Social Scrape/chatScrape.txt'
+// const logPath = '/Users/socialscrape/Social Wake Dropbox/Social Scrape/testLog.txt'
+
 
 
 const fs = require('fs')
@@ -13,10 +16,7 @@ const currentVersion = macosVersion()
 
 const messagesDb = require('./lib/messages-db.js')
 
-var linkScrape = fs.createWriteStream(linkPath, {
-    flags: 'a' // 'a' means appending (old data will be preserved)
-  })
-  var writeLink = (line) => linkScrape.write(`\n${line}`);
+
 
 function warn(str) {
     if (!process.env.SUPPRESS_OSA_IMESSAGE_WARNINGS) {
@@ -60,8 +60,9 @@ function fromAppleTime(ts) {
     if (unpackTime(ts) != 0) {
         ts = unpackTime(ts)
     }
-
-    return new Date((ts + DATE_OFFSET-14400) * 1000)
+    
+    return new Date((ts + DATE_OFFSET/*-14400*/) * 1000)
+  
 }
 
 // Since macOS 10.13 High Sierra, some timestamps appear to have extra data
@@ -189,7 +190,19 @@ function listen() {
     return emitter
 }
 
-async function getRecentChats(limit = 10) {
+async function getRecentChats(limit = 100) {
+   
+
+    var linkScrape = fs.createWriteStream(linkPath, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    
+      var chatLogger = fs.createWriteStream(logPath, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+      var writeLink = (line) => linkScrape.write(`\n${line}`);
+      var writeChatLog = (line) => chatLogger.write(`\n${line}`);
+
     const db = await messagesDb.open()
 
     const query = `
@@ -201,43 +214,46 @@ async function getRecentChats(limit = 10) {
             is_from_me
         FROM message
         LEFT OUTER JOIN handle ON message.handle_id = handle.ROWID
-        ORDER BY handle.rowid DESC
+        ORDER BY date DESC
         LIMIT ${limit};
     `
 
     const chats = await db.all(query)
 
-    for (let i = 0; i < chats.length; i++)
+    for (let i = 0; i < chats.length; i++)//loop through ${limit} chats
         {
-            if (chats[i].text.includes('tiktok.com'))
+            if (chats[i].text !== null && chats[i].text.includes("tiktok.com")/* && !chats[i].text.includes('Disliked')&& chats[i].group !== undefined && chats[i].group.includes("chat652293730519823796")*/)//if text contains tiktok.com...
             {
-
-                fs.readFile(linkPath, function (err, data) {
+                fs.readFile(linkPath, function (err, data) { //read chatScrape.txt...
                     if (err) throw err;
 
-                    if(data.includes(chats[i].text)){
-                     console.log("Link already added")
+                    if(data.includes(chats[i].text)){ //if link i is in cchatScrape.txt...
+                     console.log("Link " +i+ " already logged")
                     }
-                    else 
+                    else  //if not in chatLog, add it to chatLog
                     {
-                        fs.readFile(logPath, function (err, data2) {
-                            if (err) throw err;
-        
-                            if(data2.includes(chats[i].text)){
-                             console.log("Link already in database")
-                            }
-                            else {
-                        writeLink(chats[i].text)}
-                            });
+                        let fullDate = fromAppleTime(chats[i].date)
+                        let shortDate = fullDate.toLocaleString('en-US', {
+                            timeZone: 'America/New_York',
+                            year: "2-digit",
+                            month: '2-digit',
+                            day: '2-digit',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            second: '2-digit',
+                            // timeStyle: 'full'
+                          })
+                        console.log(shortDate);
+                         
+                         writeChatLog(`${shortDate}, ${chats[i].text}, ${chats[i].handle}`);
+                         writeLink(chats[i].text);
+                         console.log("Link Added");    
                     }
                   });
                 
-                
-                
-                
             }
-        }
-    console.log(chats[0])
+        }// end of loop
+    //console.log(chats[0])
     return chats
 }
 
